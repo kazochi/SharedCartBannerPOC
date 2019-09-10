8a7148c2db91c596696b5c82fb6e6da2b528d2c7
Kazuhito Ochiai <kochiai@grubhub.com>
2019-09-10 15:15:52 -0400

    Initial Commit

---
+++ SharedCartBannerPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SharedCartBannerPOC
+//
+//  Created by Kazuhito Ochiai on 9/10/19.
+//  Copyright © 2019 Kazuhito Ochiai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
